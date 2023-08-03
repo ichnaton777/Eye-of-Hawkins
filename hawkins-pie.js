16a2f0a22197f8404970957d535e01d3bd1cb8e2
eloquent161 <4516057-eloquent161@users.noreply.replit.com>
2023-08-03 19:28:28 +0000

    Initial commit

---
+++ hawkins-pie.js
@@ -0,0 +1,116 @@
+let h;
+// let fillColor;
+let h1000,h900, h850, h800,h700,h670,h600,h540,h500,h400,h350,h310,h250,h200,h175,h150,h125,h100,h75,h50,h30,h20lh0;
+let hData;
+
+
+
+function preload() {
+
+    hData = loadTable('data/hsb-hawkins.csv','csv','header');
+}
+
+function setup() {
+    
+    fillColor = "purple"
+    createCanvas(800, 800);
+    h=0;
+    background(180);
+    // circle(200, 200, 200)
+    colorMode(HSB)
+    angleMode(DEGREES)
+    for (let r = 0; r < hData.getRowCount(); r++ ) {
+        for (let c=0; c < hData.getRowCount(); c++ ) {
+            print(hData.get(r,c))
+        }
+    }
+
+    h1000 = new Hawkins(1000,"Full Consciousness")
+    h1000.drawPie();
+    
+    h900 = new Hawkins(900,"Supra Casual Truth");
+    h900.drawPie();
+
+    h850 = new Hawkins(850,"Divine Grace & Love");
+    h850.drawPie();
+
+    h800 = new Hawkins(800,"The Great Void")
+    h800.drawPie()
+    
+   
+    h700 = new Hawkins(700,"Enlightenment");
+    h700.drawPie();
+
+    h670 = new Hawkins(670,"Non-Duality")
+    h670.drawPie()
+
+  
+    h600 = new Hawkins(600,"Peace");
+    h600.drawPie();
+    
+    h540 = new Hawkins(540,"Oneness / Joy")
+    h540.drawPie()
+
+    h500 = new Hawkins(500,"Inner Love")
+    h500.drawPie()
+
+    h440 = new Hawkins(440,"Inner Wisdom")
+    h440.drawPie()
+    
+    h400 = new Hawkins(400,"Reason")
+    h400.drawPie();
+
+    h350 = new Hawkins(350,"Acceptance")
+    h350.drawPie();
+    
+    h310 = new Hawkins(310,"Willingness")
+    h310.drawPie();
+
+    h250 = new Hawkins(250,"Neutrality")
+    h250.drawPie();
+
+    h200 = new Hawkins(200,"Courage")
+    h200.drawPie();
+    
+    h175 = new Hawkins(175,"Pride");
+    h175.drawPie();
+
+    h150 = new Hawkins(150,"Anger")
+    h150.drawPie();
+
+    h125 = new Hawkins(125,"Desire")
+    h125.drawPie();
+    
+    h100 = new Hawkins(100,"Fear")
+    h100.drawPie();
+  
+    h75 = new Hawkins(75, "Grief")
+    h75.drawPie();
+    
+    h50 = new Hawkins(50, "Apathy");
+    h50.drawPie();
+
+    h30 = new Hawkins(30,"Guilt")
+    h30.drawPie();
+    
+    h20 = new Hawkins(20,"Shame")
+    h20.drawPie()
+
+    h0 = new Hawkins(1,"Death")
+    h0.drawPie()
+    
+    
+}
+
+function draw() {
+ /*  if(h0.hit()) {
+      //background(180)
+    //  h0.drawBigPie() 
+      console.log("hit")
+    } else {
+       h0.drawPie()
+    // console.log("nohit")
+  }   
+  */
+}
+
---
+++ hawkins.js
@@ -0,0 +1,188 @@
+class Hawkins {
+    hue;         // hawkins number 1-1000
+    emotion;     // text we set in any language possible
+    fillColor;   // tricked a bit to make it nice
+    strokeColor; // opposite of fillColor - average for quadrant
+    quadrant;    // 1,2,3,4
+    minutes;     // 0-60 in an hour
+    angle;       // number 0-360, angel starting at 12h 
+    number;      // in the list, lower start at 1 - 24
+    minutes;     // same as angle but now in clock terms 1-60
+    state;       // associated emotional state
+    life;        // view of life
+    key;         // key to transcend to next level
+    experience;  // what we experience
+    location;    // where we are
+
+    drawResponseSpot(angel, radius) {
+        // we have already swapped angel and radius
+        fill(this.fillColor);
+        noStroke();
+        push();
+        translate(width/2,height/2);
+        rotate(angel);
+        radius = (radius / 6) + 200
+        circle(radius,0,10);
+        pop();
+        
+    }
+    drawResponseSpots() {
+        // will make 24 circle spots in a circular shape, all in this color, relevant as output on this frequency
+        // the angle that we had, will now be the radius -> outRadius
+        // and we loop through the angels of the list just like that -> angels (going from 3h)
+        // typo intended
+    
+        let angels = [270,252,243,234,216,210.6,198, 187.2, 180,162, 150,135,123,105,90,75,60,45,30,15,0,150,324,306,272]
+        let a;
+        for (a=0; a < angels.length; a++) {
+            // works but fails this.drawResponseSpot(angels[a],this.angle) 
+            this.drawResponseSpot(angels[a],this.hue) 
+        }
+    }
+
+    
+    drawMainSpot() {
+        fill(this.fillColor);
+       // fill("purple")
+        noStroke();
+        push();
+        translate(width/2,height/2);
+        rotate(this.angle);
+        console.log("ja")
+        circle(180,0,20);
+        pop();
+        
+    }
+    
+    drawPie() {
+        fill(this.fillColor);
+       // strokeWeight(1)
+       //  stroke("grey");
+        noStroke();
+        arc(400, 400, 320,320 ,this.angle -2 , this.angle + 2  ,PIE);
+    }
+
+     drawBigPie() {
+        fill(this.fillColor);
+        strokeWeight(5)
+        stroke("white");
+        arc(200, 200, 200,200 ,this.angle -6 , this.angle + 6  ,PIE);
+    }
+    
+    getAngle() {
+        // based on this.hue
+        // this is a non-linear mapping
+        // first quadrant 0-49
+        // second quadrant 50 - 200
+        // 3rd quadrant    200 - 500
+        // 4th quadrant 500 - 1000 ;
+        // in earch quadrant linear mapping
+        // 12h is 0 hue and 270 degrees.
+        // 3h is 0 degrees.
+
+      //  const quadrant = this.getQuadrant(this.hue);
+        console.log("q=" + this.quadrant)
+        switch(this.quadrant) {
+            case 1:
+                return (round( 270+ 90* this.hue/50))
+                break;
+            case 2:
+                return (round(90 * (this.hue-50) /150));
+                break;
+            case 3:
+                        
+                return(90 +  90 * (this.hue - 200)/300)
+                break;
+            case 4:
+                return(180 + 90 * (this.hue - 500)/ 500)
+                break;
+            default:
+                return;
+                break;
+        }
+    }
+    
+    setHue(hue) {
+        // just return hue as is
+        return hue
+    }
+
+    getQuadrant(h) {
+        const hue = round(h) // 
+        switch(true){    // note: not switch (hue)
+            case (hue> 0 && hue < 50):
+                return 1;
+                break;
+            case (hue > 49 && hue < 201):
+                return 2;
+                break;
+            case (hue > 199 && hue < 500):
+                return 3;
+                break;
+            case (hue > 499 && hue < 1001):
+                return 4;
+                break;
+            default:
+                return ;
+                break;
+        }
+    }
+    setRealHue(hue) {
+        // hue is number 0-1000. 
+        // by quadrant, we stretch colors for human use
+       
+        switch(this.quadrant) {
+            case 1:
+                // red zone
+                // 0-30 Hue
+                // 0-49 Hawkins Hue
+                return (map (this.hue,0, 49,0,30))
+                break;
+            case 2:
+                // yellow zone
+                // 30 - 90 Hue
+                // 50 - 200 Hawkins Hue
+                return (map(this.hue,50,200,30,90))
+                break;
+            case 3:
+                // green - blue zone
+                // 90 - 270 Hue
+                // 200 - 500 Hawkins Hue
+                const val = map(this.hue,200,500,90,270)
+                console.log("3: val=" + val)
+                return val;
+                break;
+            case 4:
+                // purple zone
+                // 270 - 330 Hue - not too red!
+                // 500 - 1000 Hawkins hue
+                console.log("here")
+                return (map(this.hue,500,1000,270,330))
+                break;
+            default:
+                return ;
+                break;
+                
+        }
+    }
+    
+    hit() {
+    //  arc(200, 200, 200,200 ,this.angle -2 , this.angle + 2  ,PIE);
+        return collidePointArc (mouseX,mouseY,200,200,200,200, this.angle,3)
+        
+    }
+    
+     constructor (H,E) {
+        this.hue = H;
+        this.emotion = E;
+       
+        this.strokeColor = "white"
+        this.quadrant = this.getQuadrant(this.hue)
+        this.angle = this.getAngle();
+        this.fillColor = color(this.setRealHue(this.hue), 100,100)
+        console.log("created: " + this.emotion + "\nhue:" + this.hue + " angle:" + this.angle + " fillColor:" + this.fillColor.toString('hsb') + "\n\n")
+    }
+    
+    
+    
+}
